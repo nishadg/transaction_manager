f2f4831d4a9c3598783d8afa43d5c1881c54dcb1
Nishad Gurav <nishadsinbox@gmail.com>
2017-06-02 17:11:57 -0700

    naive flush

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,3 +1,7 @@
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 /**
  *
  */
@@ -5,17 +9,25 @@ public class Transaction {
 
     final int SUCCESS = 0;
     ILogManager logManager;
+    PolicyDBAdapter policyDB;
+    ArrayList<PolicyModel> buffer;
+    int bufferSize = 5;
     long trID;
+
+    public void setBufferSize(int bufferSize) {
+        this.bufferSize = bufferSize;
+    }
 
     long begin() {
         trID = 0; //TODO get trID from DB
         logManager = new SimpleLogManager();
+        policyDB = new PolicyDBAdapter();
+        buffer = new ArrayList<>(bufferSize);
         return trID;
     }
 
     int commit() {
-        logManager.flush(Long.MAX_VALUE);
-        //code to write policy to policy table
+        flush();
         return SUCCESS;
     }
 
@@ -23,9 +35,25 @@ public class Transaction {
         return SUCCESS;
     }
 
-    int log(PolicyModel policy) {
+    int write(String policyJSON) {
+        PolicyModel policyModel = new Gson().fromJson(policyJSON, PolicyModel.class);
+        PolicyModel policy = new PolicyModel(policyModel.policyID,
+                new java.sql.Timestamp(new java.util.Date().getTime()), null, policyJSON);
+
         logManager.write(policy, trID);
+        buffer.add(policy);
+        if (buffer.size() == bufferSize) {
+            flush();
+        }
         return SUCCESS;
+    }
+
+    private void flush() {
+        logManager.flush(Long.MAX_VALUE);
+        for (PolicyModel policy : buffer) {
+            policyDB.write(policy);// TODO check for update and write invalidate logic
+            buffer.remove(policy);
+        }
     }
 
 }
